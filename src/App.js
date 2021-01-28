99cf481d014dd7706095c4a8b7c8337d94b36849
zakaria29 <zakaria@smktelkom-mlg.sch.id>
2021-01-28 10:37:57 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { Switch, Route } from "react-router-dom";
+import Login from "./pages/Login"
+import Product from "./pages/Product"
+import Transaction from "./pages/Transaction"
+import Cart from "./pages/Cart"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component{
+  render(){
+    return(
+      <Switch>
+        <Route exact path="/" component={Product} />
+        <Route path="/login" component={Login} />
+        <Route path="/transaction" component={Transaction} />
+        <Route path="/cart" component={Cart} />
+      </Switch>
+    )
+  }
 }
-
-export default App;
